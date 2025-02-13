69b75e28a994f5e2e577c2e6a9a447e1dba7ec21
Jay Shen Tham (NCS) <jayshen.tham@ncs.com.sg>
2025-02-13 16:43:16 +0800

    shift out integration runtime to different file

--- main.datafactory.tf
+++ main.datafactory.tf
@@ -95,19 +95,3 @@ resource "azurerm_data_factory_credential_user_managed_identity" "this" {
   annotations     = each.value.annotations
   description     = each.value.description
 }
-
-resource "azurerm_data_factory_integration_runtime_self_hosted" "this" {
-  for_each = var.integration_runtime_self_hosted
-
-  data_factory_id                              = azurerm_data_factory.this.id
-  name                                         = each.value.name
-  description                                  = each.value.description
-  self_contained_interactive_authoring_enabled = each.value.self_contained_interactive_authoring_enabled
-
-  dynamic "rbac_authorization" {
-    for_each = each.value.rbac_authorization != null ? [each.value.rbac_authorization] : []
-    content {
-      resource_id = rbac_authorization.value.resource_id
-    }
-  }
-}
