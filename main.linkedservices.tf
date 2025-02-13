d89c0e2dccf9d9a698dedf8143af69db1ec0b7c1
Jay Shen Tham (NCS) <jayshen.tham@ncs.com.sg>
2025-02-13 15:03:09 +0800

    add linked service for databricks

--- main.linkedservices.tf
+++ main.linkedservices.tf
@@ -1,4 +1,4 @@
-resource "azurerm_data_factory_linked_service_azure_blob_storage" "linked_service_azure_blob_storage" {
+resource "azurerm_data_factory_linked_service_azure_blob_storage" "this" {
   for_each = var.linked_service_azure_blob_storage
 
   name                       = each.value.name
@@ -32,6 +32,60 @@ resource "azurerm_data_factory_linked_service_azure_blob_storage" "linked_servic
     content {
       linked_service_name = service_principal_linked_key_vault_key.value.linked_service_name
       secret_name         = service_principal_linked_key_vault_key.value.secret_name
+    }
+  }
+}
+
+resource "azurerm_data_factory_linked_service_databricks" "this" {
+  for_each = var.linked_service_databricks
+
+  adb_domain               = each.value.adb_domain
+  data_factory_id          = each.value.data_factory_id
+  name                     = each.value.name
+  additional_properties    = each.value.additional_properties
+  annotations              = each.value.annotations
+  description              = each.value.description
+  integration_runtime_name = each.value.integration_runtime_name
+  parameters               = each.value.parameters
+
+  # Authentication Options (Only one can be specified)
+  access_token               = each.value.access_token
+  msi_work_space_resource_id = each.value.msi_work_space_resource_id
+
+  dynamic "key_vault_password" {
+    for_each = each.value.key_vault_password != null ? [each.value.key_vault_password] : []
+    content {
+      linked_service_name = key_vault_password.value.linked_service_name
+      secret_name         = key_vault_password.value.secret_name
+    }
+  }
+
+  # Cluster Integration Options (Only one can be specified)
+  existing_cluster_id = each.value.existing_cluster_id
+
+  dynamic "instance_pool" {
+    for_each = each.value.instance_pool != null ? [each.value.instance_pool] : []
+    content {
+      instance_pool_id      = instance_pool.value.instance_pool_id
+      cluster_version       = instance_pool.value.cluster_version
+      min_number_of_workers = instance_pool.value.min_number_of_workers
+      max_number_of_workers = instance_pool.value.max_number_of_workers
+    }
+  }
+
+  dynamic "new_cluster_config" {
+    for_each = each.value.new_cluster_config != null ? [each.value.new_cluster_config] : []
+    content {
+      cluster_version             = new_cluster_config.value.cluster_version
+      node_type                   = new_cluster_config.value.node_type
+      driver_node_type            = new_cluster_config.value.driver_node_type
+      max_number_of_workers       = new_cluster_config.value.max_number_of_workers
+      min_number_of_workers       = new_cluster_config.value.min_number_of_workers
+      spark_config                = new_cluster_config.value.spark_config
+      spark_environment_variables = new_cluster_config.value.spark_environment_variables
+      custom_tags                 = new_cluster_config.value.custom_tags
+      init_scripts                = new_cluster_config.value.init_scripts
+      log_destination             = new_cluster_config.value.log_destination
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -52,18 +52,6 @@ variable "linked_service_azure_blob_storage" {
   }))
   default = {}
 
-  validation {
-    condition = alltrue([
-      for ls in values(var.linked_service_azure_blob_storage) : length(compact([
-        ls.connection_string,
-        ls.connection_string_insecure,
-        ls.sas_uri,
-        ls.service_endpoint
-      ])) <= 1
-    ])
-    error_message = "Only one of connection_string, connection_string_insecure, sas_uri, or service_endpoint may be set per linked service."
-  }
-
   description = <<DESCRIPTION
     A map of Azure Blob Storage linked services, where each key represents a unique linked service configuration.
     Each object in the map consists of the following properties:
@@ -101,4 +89,81 @@ variable "linked_service_azure_blob_storage" {
     DESCRIPTION
 }
 
+variable "linked_service_databricks" {
+  type = map(object({
+    adb_domain                 = string
+    data_factory_id            = string
+    name                       = string
+    additional_properties      = optional(map(string), null)
+    annotations                = optional(list(string), null)
+    description                = optional(string, null)
+    integration_runtime_name   = optional(string, null)
+    parameters                 = optional(map(string), null)
+    access_token               = optional(string, null)
+    msi_work_space_resource_id = optional(string, null)
+    key_vault_password = optional(object({
+      linked_service_name = string
+      secret_name         = string
+    }), null)
+    existing_cluster_id = optional(string, null)
+    instance_pool = optional(object({
+      instance_pool_id      = string
+      cluster_version       = string
+      min_number_of_workers = optional(number, 1)
+      max_number_of_workers = optional(number, null)
+    }), null)
+    new_cluster_config = optional(object({
+      cluster_version             = string
+      node_type                   = string
+      driver_node_type            = optional(string, null)
+      max_number_of_workers       = optional(number, null)
+      min_number_of_workers       = optional(number, 1)
+      spark_config                = optional(map(string), null)
+      spark_environment_variables = optional(map(string), null)
+      custom_tags                 = optional(map(string), null)
+      init_scripts                = optional(list(string), null)
+      log_destination             = optional(string, null)
+    }), null)
+  }))
+  default = {}
 
+  description = <<DESCRIPTION
+    A map of Azure Data Factory Linked Services for Databricks, where each key represents a unique configuration.
+    Each object in the map consists of the following properties:
+
+    - `adb_domain` - (Required) The domain URL of the Databricks instance.
+    - `data_factory_id` - (Required) The ID of the Data Factory where the linked service is associated.
+    - `name` - (Required) The unique name of the linked service.
+    - `additional_properties` - (Optional) Additional custom properties.
+    - `annotations` - (Optional) A list of tags to annotate the linked service.
+    - `description` - (Optional) A description of the linked service.
+    - `integration_runtime_name` - (Optional) The integration runtime reference.
+    - `parameters` - (Optional) A map of parameters.
+
+    ### Authentication Options (Only one can be set):
+    - `access_token` - (Optional) Authenticate to Databricks via an access token.
+    - `key_vault_password` - (Optional) Authenticate via Azure Key Vault. 
+      - `linked_service_name` - (Required) Name of the Key Vault Linked Service.
+      - `secret_name` - (Required) The secret storing the access token.
+    - `msi_work_space_resource_id` - (Optional) Authenticate via managed service identity.
+
+    ### Cluster Integration Options (Only one can be set):
+    - `existing_cluster_id` - (Optional) The ID of an existing cluster.
+    - `instance_pool` - (Optional) Use an instance pool. This requires a nested `instance_pool` block.
+      - `instance_pool_id` - (Required) The identifier of the instance pool.
+      - `cluster_version` - (Required) The Spark version.
+      - `min_number_of_workers` - (Optional) Minimum worker nodes (default: 1).
+      - `max_number_of_workers` - (Optional) Maximum worker nodes.
+    - `new_cluster_config` - (Optional) Create a new cluster.
+      - `cluster_version` - (Required) Spark version.
+      - `node_type` - (Required) Node type.
+      - `driver_node_type` - (Optional) Driver node type.
+      - `max_number_of_workers` - (Optional) Max workers.
+      - `min_number_of_workers` - (Optional) Min workers (default: 1).
+      - `spark_config` - (Optional) Key-value pairs for Spark configuration.
+      - `spark_environment_variables` - (Optional) Spark environment variables.
+      - `custom_tags` - (Optional) Tags for the cluster.
+      - `init_scripts` - (Optional) Initialization scripts.
+      - `log_destination` - (Optional) Log storage location.
+    DESCRIPTION
+}
